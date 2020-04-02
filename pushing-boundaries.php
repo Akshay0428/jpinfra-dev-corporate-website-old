c6abb46c6d525557f8486c30c97c3a1e1cb89d17
Shruti0398 <shrusp98@gmail.com>
2020-04-02 15:45:40 +0530

    pushing  bouundaries and north jp

--- pushing-boundaries.php
+++ pushing-boundaries.php
@@ -2,6 +2,31 @@
     <link rel="stylesheet" type="text/css" href="css/style1.css">
     <link rel="stylesheet" type="text/css" href="css/responsive1.css">
     
+
+ <style>
+    .line_human {
+        height: 2px;
+        position: absolute;
+        left: 16%;
+        width: 29%;
+        top: 143%;
+        z-index: 99;
+        background-color: #2c5686;
+    }
+    .main_absolute {
+        position: absolute;
+        left: 40%;
+        top: 118%;
+    }
+    .content_abs {
+        width: 39%;
+        background-color: #76869D;
+        padding: 25px;
+    }
+    .content_abs p {
+        margin: 20px 0px;
+    }
+ </style>   
     <a href="index.php">
         <div class="logo-banner">
     	   <img src="img/JP Infra Logo.png">
@@ -47,32 +72,184 @@ in construction.</p>
             </div>
         </div>
     </div>
-    
-    <!-- above testimonials start -->
-			<div class="abovefooter_main push-below-sec">
-				<div class="container_aks bg-3 text-center wow fadeIn" data-wow-duration="4s" style="visibility: visible; animation-duration: 4s; animation-name: fadeIn;">    
-					<h1 data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">Looking for a property,<br> but not sure where to start?</h1>
-					<div class="liveHead2" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
-						<p>Let us help. Speak to our property expert today.</p>   
-					</div>
-					<!--btn-->
-		            <div class="project-property-button ">
-		                <a href="https://www.dotwisedigital.com/jpuat/contact.php" target="blank">
-    		                <div class="know-text-project-property">
-    		                    <h5>Know More</h5>
-    		                </div>
-		                </a>
-		                <div class="know-arrow-project-property">
-		                    <img src="img/Project Button Arrow.png" class="know-arrow-property" alt="arrow">
-		                </div>
-		                <div class="clearfix"></div>
-		            </div>
-		            <div class="clearfix"></div>	
-				</div>
-				<div class="clearfix"></div>
-			</div>	
-			<!-- above testimonials ends -->
 
+
+
+<!-- Human Touch start here-->
+
+    <section class="human_sec">
+        <div class="container_main">
+            <div class="human_main">
+                <div class="main_absolute">
+                        <div class="content_abs">
+                            <p class="head_con">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <p class="para_con">"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."</p>
+                        </div>
+                </div>
+                <div class="line_human">
+                </div>
+                <div class="human_col">
+                    <h1 data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">The <br> Human Touch</h1>
+                    <!-- <div class="lable_line">
+                        <img src="img/Gray-Head-line.png" class="northimage">
+                    </div> -->
+                    <div class="padding-project" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>   
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="human-img">
+                        <img src="img/video-human.png" class="img-cls">
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="human_col mob_human">
+                    <div class="human-img">
+                        <img src="img/Playground.jpg" class="img-cls2">
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </section>
+
+
+<!-- Human Touch end here -->
+
+<!-- Carbon Foot Print Starts Here-->
+ <section class="Integral_sec">
+        
+        <div class="container_main carbon_main text-center">
+            <div class="container_main text-center">
+                <h1 data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">Our Carbon Footprint</h1>
+                <div class="liveHead2 padding-project" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
+                    <p class="carbon_txt">We at JP Infra believe in saving our environment and have ensured we follow what we believe in.</p>
+                    <!--<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>   -->
+                </div>
+                <div class="footprint-tab">
+                    <div class="foot-img-line">
+                        <img src="img/foot-line.png" alt="foot-line.png">
+                        <div class="footprint-tabs">
+                            <div class="srw-tab" id="solar">
+                                <h6 class="tc-grey srw-tab-active" id="h61">SOLAR FOR CONSTRUCTION</h6>
+                                <div class="blue-dot blue-dot-active"></div>
+                            </div>
+                            <div class="srw-tab" id="water">
+                                <h6 class="tc-grey" id="h62">RAINWATER HARVESTING</h6>
+                                <div class="blue-dot2"></div>
+                            </div>
+                            <div class="srw-tab" id="waste">
+                                <h6 class="tc-grey" id="h63">WASTE MANAGEMENT</h6>
+                                <div class="blue-dot3"></div>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        
+                        <div class="carbon-div">
+                            <div class="solar">
+                                <div class="solar-content">
+                                    <div class="solar-img">
+                                        <img src="img/solar.jpg" alt="solar">
+                                    </div>
+                                    <div class="solar-txt">
+                                        <div class="solar-img-txt">
+                                            <div class="solar-icon">
+                                                <img src="img/Solar-Icon.png" class="img-icon">
+                                            </div>
+                                            <div class="solar-para">
+                                                <h6>Solar for Construction</h6>
+                                            </div>
+                                            <div class="clearfix"></div>    
+                                        </div>
+                                        <p>We have solar-powered panels installed at construction sites that power the work done on site. It is a great boost to energy conservation along with effectiveness.</p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                            <div class="rainwater">
+                                <div class="solar-content">
+                                    <div class="solar-img">
+                                        <img src="img/solar.jpg" alt="solar">
+                                    </div>
+                                    <div class="solar-txt">
+                                        <div class="solar-img-txt">
+                                            <div class="solar-icon">
+                                                <img src="img/Solar-Icon.png" class="img-icon">
+                                            </div>
+                                            <div class="solar-para">
+                                                <h6>RAINWATER HARVESTING</h6>
+                                            </div>
+                                            <div class="clearfix"></div>    
+                                        </div>
+                                        <p>We have also employed rain-water harvesting techniques at our projects in Mira Road, that allows for water conservation and preservation. </p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                            <div class="waste">
+                                <div class="solar-content">
+                                    <div class="solar-img">
+                                        <img src="img/solar.jpg" alt="solar">
+                                    </div>
+                                    <div class="solar-txt">
+                                        <div class="solar-img-txt">
+                                            <div class="solar-icon">
+                                                <img src="img/Solar-Icon.png" class="img-icon">
+                                            </div>
+                                            <div class="solar-para">
+                                                <h6>WASTE MANAGEMENT</h6>
+                                            </div>
+                                            <div class="clearfix"></div>    
+                                        </div>
+                                        <p>We have waste management solutions in JP Infra projects to ensure that waste is disposed off correctly without harming the environment</p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="clearfix"></div>    
+                    </div>
+                </div>
+            </div>
+            
+                
+        </div>
+        
+    </section>
+
+<!-- Carbon Foot Print Ends Here -->
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script>
+
+$("document").ready(function(){
+  $(".tab-slider--body").hide();
+  $(".tab-slider--body:first").show();
+});
+
+$(".tab-slider--nav li").click(function() {
+  $(".tab-slider--body").hide();
+  var activeTab = $(this).attr("rel");
+  $("#"+activeTab).fadeIn();
+    if($(this).attr("rel") == "tab2"){
+        $('.tab-slider--tabs').addClass('slide');
+    }else{
+        $('.tab-slider--tabs').removeClass('slide');
+    }
+  $(".tab-slider--nav li").removeClass("active");
+  $(this).addClass("active");
+});
+
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.5/slick.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!--<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>-->
 
